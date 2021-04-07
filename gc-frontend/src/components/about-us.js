6b5fe7c2173a67c13276374fb48b6b29ca789ee2
dommie123 <dman9532@gmail.com>
2021-04-06 23:04:30 -0600

    Added more function components

---
+++ gc-frontend/src/components/about-us.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function AboutMe() {
+    return (
+        <div className="container-fluid" id="about-container">
+            <article>
+                <h4>Who we are: </h4>
+                <p>
+                    We are a small, but growing, community of gamers of varying backgrounds who share similar ideas
+                    and levels of passion for gaming as a whole. We play a wide variety of games, including Pokemon, 
+                    Mario Kart, Minecraft, Space Engineers, Iron Harvest, and Super Smash Bros. just to name a few. 
+                    For those of us with a competitive spirit, we also host tournaments around the games we play and
+                    give prizes to our top competitors every so often. Finally, when we're not doing either of those two
+                    things, we hang out and discuss gaming topics and dank memes. But as much of an amazing community
+                    as we are, we didn't just appear out of nowhere.
+                    <br />
+                    "So how did you guys start"? you might ask. As the author of this section, I don't know much of
+                    our origin, but I can tell you how I first became a member. It was a day like any other at 
+                    Granger High School. I had classes to take, work to do, and house affairs to take of in between.
+                    Then, as I was walking across the school's hallway, I noticed a flyer for a gaming tournament.
+                    As I saw that flyer, a flurry of thoughts rushed into my head. "A gaming tournament? In a school?!"
+                    To me, that was unheard of. Just the thought alone filled me with excitement! So without
+                    hesitation, I attended the tournament. As expected, I did poorly. Of course, my own performance
+                    didn't matter, because the energy of the room and seeing everyone else perform their
+                    best was enough for me to keep attending the club meetings. And the very next school year, 
+                    I did just that. During our regular meetings, we would play games and mingle with other 
+                    gamers. Once per quarter, we would also host tournaments to see which of us, as well as a few 
+                    other students not part of the club, would be number one. Essentially, we did much of the same
+                    things we still do today. We didn't go without our dark times, though. After game club founder, 
+                    Anthony Illif, and host (a.k.a, Granger's AP Chemistry teacher), Mr. Quist, left the school, many
+                    of us disbanded from the club and it became much more difficult to hold our regular meetings.
+                    Thankfully, the few that still saw hope that the club would live on (myself included), stayed
+                    with the club and sought to rebuild it, even after it dissolved from the school. Today, we are
+                    20+ members strong and still share the same passion for gaming, as well as the community, as we did
+                    since the beginning!
+                    <br /> 
+                </p>
+            </article>
+        </div>
+    )
+}
